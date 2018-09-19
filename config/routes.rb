04a09ec6192579d0ccb9387299b78e4e5d91e6f8
heesu <tedlee0314@gmail.com>
2018-09-20 00:21:49 +0900

    route정리, index 및 sign_in, sign_up UI 커스터마이징
    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,10 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations'}
-  resources :intros
-  resources :settings
-  # resources :messages
-  resources :faqs
+
   resources :posts
-  resources :pages
   resources :users
 
   ActiveAdmin.routes(self)
   root 'home#index'
-  get 'home/index'
-  get 'splash' => "home#splash"
-  get 'sample' => "home#sample"
-  get 'intro' => "home#intro"
-  get 'ajax_test' => "home#ajax_test"
-  get 'remote_test' => "home#remote_test"
-  get 'main' => "home#main"
-  get 'tabs' => "home#tabs"
-  get 'test' => "home#test"
-
-  get "page/:page" => "home#page"
-  # get 'messages' => "home#messages"
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
