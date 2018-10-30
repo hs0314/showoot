d6b0c619a66ed15388fe08144fc7f45ecdb62e8a
heesu <tedlee0314@gmail.com>
2018-10-30 12:51:09 +0900

    .
    .

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,21 +1,6 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
+
   def index
-  end
-
-  def sample
-    render layout: false
-  end
-
-  def ajax_test
-
-  end
-
-  def remote_test
-
-  end
-
-  def page
-    render file: "public/pages/#{params[:page]}.html"
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,45 @@
 class PostsController < ApplicationController
+  before_action :set_post, except: [:index, :new, :create]
   def index
+    posts = PostsModel.getAllPosts
+    @my_posts = posts.where(user_id: current_user.id)
+  end
+
+  def new
+    @post = PostsModel.createPost
+  end
+
+  def create
+    post = Post.new(set_post_params)
+    post.save
+
+    redirect_to posts_path
+  end
+
+  def edit
+  end
+
+  def update
+    #@post.update_attributes(set_post_params)
+    PostsModel.setPost(@post.id, set_post_params)
+    redirect_to posts_path
   end
 
   def show
-    @post = Post.find params[:id]
+    @post = PostsModel.getPost(params[:id])
+  end
+
+  def destroy
+    PostsModel.destroyPost(@post.id)
+    redirect_to posts_path
+  end
+
+  private
+  def set_post_params
+    params.require(:post).permit(:user_id, :title, :body)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
