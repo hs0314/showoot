58ff1be33ff3da5aff21c83f62b327517c7dc214
heesu <tedlee0314@gmail.com>
2018-10-30 12:56:09 +0900

    .
    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+  ActiveAdmin.routes(self)
   devise_for :admin_users, ActiveAdmin::Devise.config
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations'}
 
   resources :posts
-  resources :users
 
-  ActiveAdmin.routes(self)
-  root 'home#index'
+  root 'posts#index'
 end
