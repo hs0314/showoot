a2f324fd558e79f5dd4b95c3d6a47690f97f542c
heesu <tedlee0314@gmail.com>
2018-10-30 12:49:53 +0900

    필요한 파일 추가, post기능 뼈대 구현
    .

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,10 +1,11 @@
 // Dom7
-var $ = Dom7;
+var $$ = Dom7;
 // Init App
 var app = new Framework7({
   id: 'com.showoot',
   root: '#app',
   theme: 'md',
+  init: false,
   view: {
     // 여러 페이지를 캐시해서 뒤로가기가 여러 depth에서도 동작하게 하려면 필요한 옵션
     stackPages: true,
@@ -18,56 +19,49 @@ var app = new Framework7({
     title: 'My App',
     closeTimeout: 3000,
   },
-  panel: {
-    swipe: 'left',
-  },
-  // touch:{
-  //   disableContextMenu: false
-  // },
-  data: function () {
-    return {
-      user: {
-        firstName: 'Seong Hoon',
-        lastName: 'Lee',
-      },
-    };
-  },
-  methods: {
-    helloWorld: function () {
-      app.dialog.alert('Hello World!');
-    },
-  },
   routes: routes,
-  vi: {
-    placementId: 'pltd4o7ibb9rc653x14',
-  },
-});
-var toast = app.toast.create({
-  title: 'John Doe',
-  text: 'Hello, how are you?',
-  on: {
-    opened: function () {
-      console.log('Toast opened')
-    }
-  }
 });
 
-var notification = app.notification.create({
-  text: 'Hello, how are you?',
-  on: {
-    opened: function () {
-      console.log('Toast opened')
+/////////////////////커스터마이징 캘린더
+function customizingCalendar(){
+  var monthNames=['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'];
+  var today = new Date();
+  var calendarInline = app.calendar.create({
+    containerEl: '#demo-calendar-inline-container',
+    value: [today],
+    weekHeader: false,
+    renderToolbar: function () {
+      return '<div class="toolbar calendar-custom-toolbar no-shadow">' +
+        '<div class="toolbar-inner">' +
+          '<div class="left">' +
+            '<a href="#" class="link icon-only"><i class="icon icon-back ' + (app.theme === 'md' ? 'color-black' : '') + '"></i></a>' +
+          '</div>' +
+          '<div class="center"></div>' +
+          '<div class="right">' +
+            '<a href="#" class="link icon-only"><i class="icon icon-forward ' + (app.theme === 'md' ? 'color-black' : '') + '"></i></a>' +
+          '</div>' +
+        '</div>' +
+      '</div>';
+    },
+    on: {
+      init: function (c) {
+        $$('.calendar-custom-toolbar .center').text(monthNames[c.currentMonth] +', ' + c.currentYear);
+        $$('.calendar-custom-toolbar .left .link').on('click', function () {
+          calendarInline.prevMonth();
+        });
+        $$('.calendar-custom-toolbar .right .link').on('click', function () {
+          calendarInline.nextMonth();
+        });
+      },
+      monthYearChangeStart: function (c) {
+        $$('.calendar-custom-toolbar .center').text(monthNames[c.currentMonth] +', ' + c.currentYear);
+      }
     }
-  }
-})
-
-$("#ajax_test").on("click", function() {
-  app.request.get("/ajax_test.js", function(data) {
-    eval(data);
-    // $("#ajax_test .item-title").text(data);
   });
-})
+}
 
-// toast.open();
-// notification.open();
-// app.methods.helloWorld()
+$$(document).on("page:init", ".page[data-name='posts-index']", function() {
+  customizingCalendar();
+});
+
+app.init();
--- db/schema.rb
+++ db/schema.rb
@@ -51,43 +51,6 @@ ActiveRecord::Schema.define(version: 2018_07_05_072042) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table "faqs", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.integer "position"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["position"], name: "index_faqs_on_position"
-  end
-
-  create_table "intros", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.string "image"
-    t.integer "position"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["position"], name: "index_intros_on_position"
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.integer "user_id"
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_messages_on_user_id"
-  end
-
-  create_table "pages", force: :cascade do |t|
-    t.string "title"
-    t.string "name"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_pages_on_name"
-  end
-
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
     t.string "title"
@@ -96,19 +59,6 @@ ActiveRecord::Schema.define(version: 2018_07_05_072042) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
-  end
-
-  create_table "settings", force: :cascade do |t|
-    t.string "title"
-    t.string "name"
-    t.text "body"
-    t.string "image"
-    t.string "url"
-    t.integer "position"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_settings_on_name"
-    t.index ["position"], name: "index_settings_on_position"
   end
 
   create_table "users", force: :cascade do |t|
